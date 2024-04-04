c616bd36a16120a4bfc6bbb90136413bfecd03d5
Sumit Kumar <j4sumit@gmail.com>
2024-04-04 12:46:22 +0530

    Intrepoloation example

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,18 +6,29 @@ import { Component, OnInit } from '@angular/core';
   selector: '[app-test]',
   // templateUrl: './test.component.html',
   template: `<h1>
-    heading form internal template
-  </h1>`,
+    <!-- String Interpolation -->
+    {{"welcome"+ name}} 
+  </h1>
+  <h2>{{4+2}}</h2>
+  <h4>{{name.length}}</h4>
+  <h5>{{name.toUpperCase()}}</h5>
+  <h2>{{greetUser()}}</h2>
+  <h4>{{siteUrl}}</h4>
+  `,
   // styleUrls: ['./test.component.css']
   styles: [`
     h1{color:red}
   `]
 })
 export class TestComponent implements OnInit {
-
+name ="Amit Chaurasiya"
+siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  greetUser(){
+    return "Hello" +this.name
   }
 
 }
