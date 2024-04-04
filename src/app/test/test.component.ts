95b1a84bf0130e3c9d09d6ad68c0eab0bcc4ba5b
Sumit Kumar <j4sumit@gmail.com>
2024-04-04 13:59:56 +0530

    Style Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,39 +5,23 @@ import { Component, OnInit } from '@angular/core';
   template: `<h1>
     {{"welcome"+ name}} 
   </h1>
-  <h2 class="text-special">Practice to successed in Accenture 1</h2>
-  <h2 [class]="successClass">Practice to successed in Accenture 2</h2>
-  <h2 class="text-special" [class]="successClass">Practice to successed in Accenture 3</h2>
-  <h2 class="text-special" [class]="successClass">Practice to successed in Accenture 4</h2>
-  <!-- for only single class -->
-  <h2 [class.text-danger]="hasError">Practice to successed in Accenture 5</h2>
-  <!-- for multiple class -->
-  <h2 [ngClass]="messageClasses">Practice to successed in Accenture 6</h2>
-  
+  <h2 [style.color]="'green'">Practiced to success in Accenture</h2>
+  <h2 [style.color]='hasError? "red" : "green"'>Practiced to success in Accenture1</h2>
+  <h2 [style.color]="highlightColor">Practiced to success in Accenture2</h2>
+  <!-- for multiple style -->
+  <h2 [ngStyle]="titleStyle">Practiced to success in Accenture3</h2>  
   `,
-  styles: [`
-.text-success{
-  color:green;
-}
-.text-danger{
-  color:red;
-}
-.text-special{
-  font-style:italic;
-}
-`]
+  styles: []
 })
 export class TestComponent implements OnInit {
 name ="Amit Chaurasiya"
-successClass="text-success"
-hasError= true;
+hasError= false;
 isSpecial=true;
-messageClasses ={
-  "text-success": !this.hasError,
-  "text-danger" : this.hasError,
-  "text-special" : this.isSpecial
+highlightColor="blue";
+titleStyle={
+  color:"red",
+  fontStyle:"italic"
 }
-
   constructor() { }
 
   ngOnInit(): void {
