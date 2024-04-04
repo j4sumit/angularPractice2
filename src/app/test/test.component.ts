813ccb66c4d0283836ec3353727a792ae058c94e
Sumit Kumar <j4sumit@gmail.com>
2024-04-04 16:18:45 +0530

    Template Reference Variables

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,41 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h1>
+  template: `<p>
     {{"welcome"+ name}} 
-  </h1>
-  <button (click)="onClick()">Submit</button>
-  {{greeting}}
-  <button (click)="onClick1($event)">Submit1</button>
-  {{eventtype}}
-
-  <button (click)="greeting='Welcome using new method type'">Submit1</button>//new type of method
-  <!-- for multiple style -->
+  </p>
+  <input type="text" #myInput placeholder="Enter Text">
+  <button (click)="logMessage(myInput)" >Log</button>
+  <button (click)="logMessage(myInput.value)" >LogValue</button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
 name ="Amit Chaurasiya"
-hasError= false;
-isSpecial=true;
-highlightColor="blue";
-titleStyle={
-  color:"red",
-  fontStyle:"italic"
-}
-greeting=""
-eventtype=""
-
   constructor() { }
 
   ngOnInit(): void {
   }
-  onClick() {
-    this.greeting="Sumit Chaurasiya"
-    }
-    onClick1(event:any) {
-      console.log(event)
-      this.eventtype=event.type
-      }
+  logMessage(data:any){
+console.log(data);
+  }
 }
