7db54b38d232a6a16eb2e0ed4e9f4f8c1fa71bac
Sumit Kumar <j4sumit@gmail.com>
2024-04-04 16:39:03 +0530

    Two way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,18 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<p>
-    {{"welcome"+ name}} 
-  </p>
-  <input type="text" #myInput placeholder="Enter Text">
-  <button (click)="logMessage(myInput)" >Log</button>
-  <button (click)="logMessage(myInput.value)" >LogValue</button>
+  template: `
+  <input type="text"  placeholder="Enter Text" [(ngModel)]="name">
+  {{name}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-name ="Amit Chaurasiya"
+  name= ""; 
   constructor() { }
 
   ngOnInit(): void {
