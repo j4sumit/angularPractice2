1ffa96ec3d53dbdf7c18e7d7c17b5b90cd4adc07
Sumit Kumar <j4sumit@gmail.com>
2024-04-05 11:53:41 +0530

    ngSwitch Directives

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,35 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-<h2 *ngIf="true">Accenture Practice</h2>
-<!-- EX :-1 -->
-<!-- <h2 *ngIf="displayStatus">Accenture Practice2</h2> -->
-
-<!-- Ex-2 -->
-<!-- <h2 *ngIf ="displayStatus else elseBlock" >if Block</h2>
-<ng-template #elseBlock> Else block</ng-template> -->
-
- <!-- Ex-3 -->
-<div *ngIf="displayStatus; then  ifBlock else elseBlock"> </div>
-<ng-template #ifBlock>
-  <h1>If Block code </h1>
-</ng-template>
-<ng-template #elseBlock>
-  <h1>else Block code</h1>
-</ng-template>
-
+<div [ngSwitch]="color" >
+<div *ngSwitchCase="'red'">yor have choosed Red</div>
+<div *ngSwitchCase="'green'">yor have choosed Green</div>
+<div *ngSwitchCase="'yellow'">yor have choosed Yellow</div>
+<div *ngSwitchCase="'pink'">yor have choosed Pink</div>
+<div *ngSwitchDefault="">Invalid Selection</div>
+</div>
   `,
   styles: [],
 })
 
 
-
-
-
 export class TestComponent implements OnInit {
 
   name = '';
-  displayStatus=true;
+  color = "Pink";
   constructor() {}
 
   ngOnInit(): void {}
