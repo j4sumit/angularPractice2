b92fbe31ef47780e4813631edc066f78659830c6
Sumit Kumar <j4sumit@gmail.com>
2024-04-05 12:26:50 +0530

    send data from Parent to child using input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPractice2';
+  name ="Sumit Chaurasiya"
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `
-<!-- <div *ngFor="let color of colors index as i"> -->
-<!-- <div *ngFor="let color of colors first as f"> -->
-<!-- <div *ngFor="let color of colors last as l"> -->
-<!-- <div *ngFor="let color of colors even as e"> -->
-<div *ngFor="let color of colors odd as o">
-  <h1>{{o}} {{color}}</h1>
-</div>
+<h1>{{receivedData}}</h1>
   `,
   styles: [],
 })
 
 export class TestComponent implements OnInit {
 
-  colors =["red","green", "yellow","blue","orang"]
+// @Input() public parentData: any;
+@Input('parentData') public receivedData: any;
+
   constructor() {}
 
   ngOnInit(): void {}
