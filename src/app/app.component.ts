0cf73c2811d935846604517a0f55adc56c289e2d
Sumit Kumar <j4sumit@gmail.com>
2024-04-05 12:53:06 +0530

    child to parent data using output decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularPractice2';
   name ="Sumit Chaurasiya"
+  message=""
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input, EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `
 <h1>{{receivedData}}</h1>
+<button (click)="firstEvent()">ChildToParentData</button>
   `,
   styles: [],
 })
@@ -13,10 +14,17 @@ export class TestComponent implements OnInit {
 // @Input() public parentData: any;
 @Input('parentData') public receivedData: any;
 
+@Output() childEvent = new EventEmitter();
+
+
   constructor() {}
 
   ngOnInit(): void {}
-  logMessage(data: any) {
-    console.log(data);
+  // logMessage(data: any) {
+  //   console.log(data);
+  // }
+  firstEvent(){
+    this.childEvent.emit("Data Received from Child")
   }
+
 }
