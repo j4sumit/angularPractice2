151d03f70102ea0a49fe052fd1f3d9527640af47
Sumit Kumar <j4sumit@gmail.com>
2024-04-05 11:42:19 +0530

    ngIf example

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  <input type="text"  placeholder="Enter Text" [(ngModel)]="name">
-  {{name}}
+<h2 *ngIf="true">Accenture Practice</h2>
+<!-- EX :-1 -->
+<!-- <h2 *ngIf="displayStatus">Accenture Practice2</h2> -->
+
+<!-- Ex-2 -->
+<!-- <h2 *ngIf ="displayStatus else elseBlock" >if Block</h2>
+<ng-template #elseBlock> Else block</ng-template> -->
+
+ <!-- Ex-3 -->
+<div *ngIf="displayStatus; then  ifBlock else elseBlock"> </div>
+<ng-template #ifBlock>
+  <h1>If Block code </h1>
+</ng-template>
+<ng-template #elseBlock>
+  <h1>else Block code</h1>
+</ng-template>
+
   `,
-  styles: []
+  styles: [],
 })
+
+
+
+
+
 export class TestComponent implements OnInit {
 
-  name= ""; 
-  constructor() { }
+  name = '';
+  displayStatus=true;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  logMessage(data:any){
-console.log(data);
+  ngOnInit(): void {}
+  logMessage(data: any) {
+    console.log(data);
   }
 }
